8134e7d8e5b9a0cd9f5a9c99c417005b3b0a8e3a
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-07-31 17:20:27 +0700

    add: update layer auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,60 +1,26 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';
-import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+import { authService } from '../services/auth.service';
+import { ResponseUtil } from '../utils/response';
 
-const prisma = new PrismaClient();
+class AuthController {
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
 
-export const signin = async (req: Request, res: Response): Promise<Response | any> => {
-  const { email, password } = req.body;
-
-  try {
-    const checkUser = await prisma.user.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (!checkUser) {
-      return res.status(404).json({
-        status: 404,
-        message: 'Account not found',
-        data: null,
-      });
+    try {
+      const handleLogin = await authService.login({ email, password });
+      res.json(
+        ResponseUtil.success({
+          id: handleLogin.user.id,
+          fullname: handleLogin.user.fullname,
+          email: handleLogin.user.email,
+          role: handleLogin.user.role,
+          access_toke: handleLogin.jwtToken,
+        })
+      );
+    } catch (error) {
+      next(error);
     }
-
-    const checkPassword = bcrypt.compareSync(password, checkUser.password);
-    if (!checkPassword) {
-      return res.status(400).json({
-        status: 400,
-        message: 'Invalid password',
-        data: null,
-      });
-    }
-
-    const user = {
-      id: checkUser.id,
-      fullname: checkUser.fullname,
-      email: checkUser.email,
-      role: checkUser.role,
-    };
-
-    const token = jwt.sign(user, process.env.SECRET_KEY as string);
-
-    return res.status(400).json({
-      status: 400,
-      message: 'Login successfully',
-      data: {
-        id: user.id,
-        role: user.role,
-        access_token: token,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      status: 500,
-      message: 'Internal server error. Please try again later.',
-      data: null,
-    });
   }
-};
+}
+
+export const authController = new AuthController();
